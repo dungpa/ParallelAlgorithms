125477b5034895bb3b4b90a2d6226eda69f6ae0a
Phan Anh Dung <phananhdung309@yahoo.com>
2011-11-01 17:36:58 +0100

    First try with BootyDivider: Using locks here is not as good as using explicit arrays. Need to reduce resource usage soon.

---
+++ BootyDivision.fs
@@ -0,0 +1,125 @@
+﻿module ParallelAlgorithms.BootyDivision
+
+open System
+open System.Threading.Tasks
+
+// Parallelization of booty division problem.
+// See details of discussion at http://www.devx.com/dotnet/Article/39219/0/pdo/D9BF62877FACB1F6EDB86291994B1873:8716/page/3.
+type BootyDivider(_itemValues) =
+    let itemValues = Array.copy _itemValues // Not really necessary.
+    let numItems = Array.length itemValues
+
+    // Important values
+    let size = 2
+    let mutable depth = 0
+
+    // Could use bool array, but use byte to extend for more than 2 dividers.
+    let path = Array.create numItems 0uy
+    let bestPath = Array.create numItems 0uy
+
+    let totalValues = Array.create size 0    
+    let bestTotalValues = Array.create size 0 
+
+    let mutable totalUnassigned = Array.sum itemValues
+    let mutable bestDifference = Int32.MaxValue
+
+    // Divide nth item to index-th person.
+    member x.select(index) =        
+        path.[depth] <- byte(index)
+        totalValues.[index] <- totalValues.[index] + itemValues.[depth]
+        depth <- depth + 1
+
+    member x.deselect(index) =        
+        depth <- depth - 1
+        path.[depth] <- 0uy
+        totalValues.[index] <- totalValues.[index] - itemValues.[depth]
+
+    member x.divide() =
+        if depth >= numItems then
+            if abs(totalValues.[0] - totalValues.[1]) < bestDifference then
+                Array.Copy(totalValues, bestTotalValues, size)
+                bestDifference <- abs(totalValues.[0] - totalValues.[1])
+            else ()            
+        else            
+            for i in 0..1 do
+                x.select(i)
+                x.divide() |> ignore
+                x.deselect(i)                    
+        bestDifference
+
+    member x.selectBB(index) =
+        totalUnassigned <- totalUnassigned - itemValues.[depth]
+        x.select(index)
+
+    member x.deselectBB(index) =        
+        x.deselect(index)
+        totalUnassigned <- totalUnassigned + itemValues.[depth]
+
+    // Using branch and bound method to prune unused branches.
+    member x.divideBB() =
+        if depth >= numItems then
+            if abs(totalValues.[0] - totalValues.[1]) < bestDifference then
+                Array.Copy(totalValues, bestTotalValues, size)
+                bestDifference <- abs(totalValues.[0] - totalValues.[1])
+            else ()            
+        elif abs(totalValues.[0] - totalValues.[1]) - totalUnassigned <= bestDifference then                       
+            for i in 0..1 do
+                x.selectBB(i)
+                x.divideBB() |> ignore
+                x.deselectBB(i)                    
+        else ()
+        bestDifference
+
+let solve(itemValues) =
+    let bd = new BootyDivider(itemValues)
+    bd.divide()
+
+let solveBB(itemValues) =
+    let bd = new BootyDivider(itemValues)
+    bd.divideBB()
+
+let inline toBinaryList n = 
+    let rec util(n, acc) =
+        match n with
+        | 0 -> acc
+        | _ -> util(n/2, n%2::acc)
+    util(n, [])
+
+let solve2(itemValues, level) =
+    let count = pown 2 level
+    // Setup different booty dividers with different configurations.
+    let bds = Array.init count (fun _ -> new BootyDivider(itemValues))
+    for i in 0..count-1 do
+        let ls = toBinaryList i
+        List.iter (fun j -> bds.[i].select(j)) ls
+
+    let solutions = Array.zeroCreate count
+    for i in 0..count-1 do
+        solutions.[i] <- bds.[i].divide()
+    Array.min solutions
+
+let solveParallel(itemValues, level) =
+    let count = pown 2 level
+    // Setup different booty dividers with different configurations.
+    let bds = Array.init count (fun _ -> new BootyDivider(itemValues))
+    for i in 0..count-1 do
+        let ls = toBinaryList i
+        List.iter (fun j -> bds.[i].select(j)) ls
+
+    let solutions = Array.zeroCreate count
+    Parallel.For(0, count,
+        fun i -> solutions.[i] <- bds.[i].divideBB()) |> ignore
+    Array.min solutions
+
+let solveBBParallel(itemValues, level) =
+    let count = pown 2 level
+    // Setup different booty dividers with different configurations.
+    let bds = Array.init count (fun _ -> new BootyDivider(itemValues))
+    for i in 0..count-1 do
+        let ls = toBinaryList i
+        List.iter (fun j -> bds.[i].select(j)) ls
+
+    let solutions = Array.zeroCreate count
+    Parallel.For(0, count,
+        fun i -> solutions.[i] <- bds.[i].divideBB()) |> ignore
+    Array.min solutions
---
+++ BootyDivision.fsx
@@ -0,0 +1,19 @@
+﻿#load "BootyDivision.fs"
+
+open ParallelAlgorithms.BootyDivision
+
+let minValue = 30
+let maxValue = 50
+let numItems = 25
+let level = 5
+let rnd = new System.Random()
+let itemValues = Array.init numItems (fun _ -> rnd.Next(minValue, maxValue+1));;
+
+#time "on";;
+
+let c1 = solve(itemValues);;
+let c11 = solve2(itemValues, level);;
+let c2 = solveParallel(itemValues, level);;
+
+let c3 = solveBB(itemValues);;
+let c4 = solveBBParallel(itemValues, level);;
